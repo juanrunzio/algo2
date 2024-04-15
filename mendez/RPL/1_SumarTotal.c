193374fa343a3e002d7ff4fc46ad8b3e17be15ac
juanchitho <jbauti9@gmail.com>
2024-04-15 14:32:39 -0300

    ejercicios de la guia 0 de RPL

--- mendez/RPL/1_SumarTotal.c
+++ mendez/RPL/1_SumarTotal.c
@@ -4,3 +4,15 @@ Empecemos con algo simple, pero que vas a tener que hacer mucho a lo largo de la
 Aprovechemos y repasemos los diferentes tipos de datos que C provee. El primero es int, que se usa para representar números enteros. Acá te dejamos la firma de una función que recibe un vector de 10 enteros y su tope. Debe devolver la suma de los elementos del vector.
 
 */
+#define MAX_ELEMENTOS 10
+
+//Devuelve la suma de los elementos del vector, desde la posición 0 hasta el tope
+int suma_de_elementos(int vector[MAX_ELEMENTOS], int tope){
+    //escribe tu codigo acá
+ int i;
+ int suma=0;
+ for(i=0; i<tope; i++){
+   suma+=vector[i];
+ }
+ return suma;
+}
---
+++ mendez/RPL/2_DondeEstaLaLetra.h
@@ -0,0 +1,12 @@
+#ifndef 2_DONDEESTALALETRA_H_
+#define 2_DONDEESTALALETRA_H_
+
+#include <stdbool.h>
+#define MAX_VECTOR 7
+#define ERROR -1
+
+int posicion_letra(char vector[MAX_VECTOR], int tope, char letra);
+
+
+
+#endif // 2_DONDEESTALALETRA_H_
