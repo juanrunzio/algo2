a61230c3d103a7bc3355acc25c6485b96cf1f7a4
juanchitho <jbauti9@gmail.com>
2024-05-06 11:31:48 -0300

    arreglo la falla de quitar los elementos y agregro lo de destruir el abb para que se vaya el error de perdida de memoria del valgrind

--- mendez/tp2-ABB-mio/pruebas_alumno.c
+++ mendez/tp2-ABB-mio/pruebas_alumno.c
@@ -3,7 +3,6 @@
 #include "src/abb_estructura_privada.h"
 #include <stdio.h>
 
-
 void prueba_simple()
 {
 	int i = 14;
@@ -18,19 +17,22 @@ int comparador(void *elemento_1, void *elemento_2)
 	return (int)(*e_1 - *e_2);
 }
 
-
 void prueba_de_crear_un_arbol_con_el_comparador_null()
 {
-	pa2m_afirmar(abb_crear(NULL) == NULL, "No se puede crear el arbol , ya que tiene un comparador NULL");
+	pa2m_afirmar(
+		abb_crear(NULL) == NULL,
+		"No se puede crear el arbol , ya que tiene un comparador NULL");
 }
-
 
 void prueba_para_comprobar_que_el_arbol_fue_creado_correctamente_y_con_las_estructuras_inicializadas()
 {
 	abb_t *nuevo_arbol = abb_crear(comparador);
-	pa2m_afirmar(nuevo_arbol != NULL, "La memoria se reservo para el nuevo arbol");
-	pa2m_afirmar(abb_tamanio(nuevo_arbol) == 0, "Como se creo el arbol la cantidad de elementos es 0");
-	pa2m_afirmar(nuevo_arbol->nodo_raiz == NULL, "Nodo raiz es igual a NULL");
+	pa2m_afirmar(nuevo_arbol != NULL,
+		     "La memoria se reservo para el nuevo arbol");
+	pa2m_afirmar(abb_tamanio(nuevo_arbol) == 0,
+		     "Como se creo el arbol la cantidad de elementos es 0");
+	pa2m_afirmar(nuevo_arbol->nodo_raiz == NULL,
+		     "Nodo raiz es igual a NULL");
 	abb_destruir(nuevo_arbol);
 }
 
@@ -43,15 +45,25 @@ void prueba_de_insertar_elementos_en_un_arbol_y_chequeo_de_tamanio()
 	int numero_2 = 2;
 	abb_t *nuevo_arbol = abb_crear(comparador);
 	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_28);
-	pa2m_afirmar(nuevo_arbol->tamanio == 1, "Se pudo insertar el primer elemento en un arbol que estaba vacio, aumento su tamanio a 1");
+	pa2m_afirmar(
+		nuevo_arbol->tamanio == 1,
+		"Se pudo insertar el primer elemento en un arbol que estaba vacio, aumento su tamanio a 1");
 	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_33);
-	pa2m_afirmar(nuevo_arbol->tamanio == 2, "Se pudo insertar el segundo elemento en el arbol, aumento su tamanio a 2");
+	pa2m_afirmar(
+		nuevo_arbol->tamanio == 2,
+		"Se pudo insertar el segundo elemento en el arbol, aumento su tamanio a 2");
 	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_4);
-	pa2m_afirmar(nuevo_arbol->tamanio == 3, "Se pudo insertar el tercer elemento en el arbol, aumento su tamanio a 3");
+	pa2m_afirmar(
+		nuevo_arbol->tamanio == 3,
+		"Se pudo insertar el tercer elemento en el arbol, aumento su tamanio a 3");
 	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_52);
-	pa2m_afirmar(nuevo_arbol->tamanio == 4, "Se pudo insertar el cuarto elemento en el arbol, aumento su tamanio a 4");
+	pa2m_afirmar(
+		nuevo_arbol->tamanio == 4,
+		"Se pudo insertar el cuarto elemento en el arbol, aumento su tamanio a 4");
 	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_2);
-	pa2m_afirmar(nuevo_arbol->tamanio == 5, "Se pudo insertar el quinto elemento en el arbol, aumento su tamanio a 5");
+	pa2m_afirmar(
+		nuevo_arbol->tamanio == 5,
+		"Se pudo insertar el quinto elemento en el arbol, aumento su tamanio a 5");
 
 	abb_destruir(nuevo_arbol);
 }
@@ -60,14 +72,18 @@ void prueba_de_insertar_un_elemento_null_en_un_arbol()
 {
 	abb_t *arbol = abb_crear(comparador);
 	abb_t *nuevo_arbol = abb_insertar(arbol, NULL);
-	pa2m_afirmar(nuevo_arbol == NULL, "No se puede insertar el elemento , ya que el elemento es NULL");
+	pa2m_afirmar(
+		nuevo_arbol == NULL,
+		"No se puede insertar el elemento , ya que el elemento es NULL");
 	abb_destruir(nuevo_arbol);
 	abb_destruir(arbol);
 }
 void prueba_de_insertar_elemento_en_un_arbol_que_es_null()
 {
 	int numero = 31;
-	pa2m_afirmar(abb_insertar(NULL, &numero) == NULL, "El arbol es NULL, por lo tanto no se puede insertar ningun elemento");
+	pa2m_afirmar(
+		abb_insertar(NULL, &numero) == NULL,
+		"El arbol es NULL, por lo tanto no se puede insertar ningun elemento");
 }
 
 void prueba_de_eliminar_un_elemento_del_arbol()
@@ -76,17 +92,21 @@ void prueba_de_eliminar_un_elemento_del_arbol()
 	int numero_33 = 33;
 	int numero_4 = 4;
 	abb_t *nuevo_arbol = abb_crear(comparador);
-	abb_insertar(nuevo_arbol, &numero_28);//ver si hace falta la asignacion
-	abb_insertar(nuevo_arbol, &numero_33);
-	abb_insertar(nuevo_arbol, &numero_4);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_28);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_33);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_4);
 	//cambiar mnsj
-	pa2m_afirmar(abb_quitar(nuevo_arbol, &numero_4) == &numero_4, "eliminado");
-	pa2m_afirmar(abb_quitar(nuevo_arbol, &numero_33) == &numero_33, "eliminado");
-	pa2m_afirmar(abb_quitar(nuevo_arbol, &numero_28) == &numero_28, "eliminado");
+	pa2m_afirmar(abb_quitar(nuevo_arbol, &numero_4) == &numero_4,
+		     "Elimino el ultimo elemento insertado");
+	pa2m_afirmar(abb_quitar(nuevo_arbol, &numero_33) == &numero_33,
+		     "Elimino el anteultimo elemento insertado");
+	pa2m_afirmar(abb_quitar(nuevo_arbol, &numero_28) == &numero_28,
+		     "Elimino el primer elemento insertado");
 
-	pa2m_afirmar(abb_tamanio(nuevo_arbol) == 0, "tamanio");
+	pa2m_afirmar(
+		abb_tamanio(nuevo_arbol) == 0,
+		"El tamaño se redujo a 0, ya que al sacarle todos los elementos quedo vacio");
 	abb_destruir(nuevo_arbol);
-
 }
 
 void prueba_de_eliminar_un_elemento_que_no_existe_en_el_arbol()
@@ -95,9 +115,11 @@ void prueba_de_eliminar_un_elemento_que_no_existe_en_el_arbol()
 	int numero_2 = 2;
 	abb_t *nuevo_arbol = abb_crear(comparador);
 	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_52);
-	pa2m_afirmar(abb_quitar(nuevo_arbol, &numero_2) == NULL, "El elemento no se encuentra en el arbol, por lo tanto no se puede eliminar");
+	pa2m_afirmar(
+		abb_quitar(nuevo_arbol, &numero_2) == NULL,
+		"El elemento no se encuentra en el arbol, por lo tanto no se puede eliminar");
+	abb_destruir(nuevo_arbol);
 }
-
 
 int main()
 {
@@ -105,28 +127,32 @@ int main()
 		"\n======================== XXX ========================");
 	prueba_simple();
 
-	pa2m_nuevo_grupo("\n======================== XXX ========================");
+	pa2m_nuevo_grupo(
+		"\n======================== XXX ========================");
 
 	pa2m_nuevo_grupo("Pruebas de la creacion de un ABB");
 	prueba_de_crear_un_arbol_con_el_comparador_null();
 	prueba_para_comprobar_que_el_arbol_fue_creado_correctamente_y_con_las_estructuras_inicializadas();
 
-	pa2m_nuevo_grupo("\n======================== XXX ========================");
+	pa2m_nuevo_grupo(
+		"\n======================== XXX ========================");
 	pa2m_nuevo_grupo("Pruebas del tipo de insercion al ABB");
 	prueba_de_insertar_elementos_en_un_arbol_y_chequeo_de_tamanio();
 	prueba_de_insertar_un_elemento_null_en_un_arbol();
 	prueba_de_insertar_elemento_en_un_arbol_que_es_null();
 
-	pa2m_nuevo_grupo("\n======================== XXX ========================");
+	pa2m_nuevo_grupo(
+		"\n======================== XXX ========================");
 	pa2m_nuevo_grupo("Pruebas de eliminacion del ABB");
 	prueba_de_eliminar_un_elemento_del_arbol();
 	prueba_de_eliminar_un_elemento_que_no_existe_en_el_arbol();
 
+	pa2m_nuevo_grupo(
+		"\n======================== XXX ========================");
 
-	pa2m_nuevo_grupo("\n======================== XXX ========================");
-
-
-	pa2m_nuevo_grupo("\n======================== XXX ========================");
-	pa2m_nuevo_grupo("\n======================== XXX ========================");
+	pa2m_nuevo_grupo(
+		"\n======================== XXX ========================");
+	pa2m_nuevo_grupo(
+		"\n======================== XXX ========================");
 	return pa2m_mostrar_reporte();
 }
--- mendez/tp2-ABB-mio/src/abb.c
+++ mendez/tp2-ABB-mio/src/abb.c
@@ -2,25 +2,26 @@
 #include "abb_estructura_privada.h"
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h> //borrar
 
 abb_t *abb_crear(abb_comparador comparador)
 {
-	if(comparador == 0)
+	if (comparador == 0)
 		return NULL;
 	abb_t *nuevo_arbol = malloc(sizeof(abb_t));
-	if(nuevo_arbol == 0)
+	if (nuevo_arbol == 0)
 		return NULL;
 	nuevo_arbol->nodo_raiz = NULL;
 	nuevo_arbol->comparador = comparador;
 	nuevo_arbol->tamanio = 0;
 	return nuevo_arbol;
-
 }
-nodo_abb_t *inserto_elemento_y_comparo(abb_t *arbol, nodo_abb_t *nodo_actual, void *elemento)
+nodo_abb_t *inserto_elemento_y_comparo(abb_t *arbol, nodo_abb_t *nodo_actual,
+				       void *elemento)
 {
-	if(nodo_actual == 0) {
+	if (nodo_actual == 0) {
 		nodo_abb_t *nuevo_nodo = malloc(sizeof(nodo_abb_t));
-		if(nuevo_nodo == 0)
+		if (nuevo_nodo == 0)
 			return NULL;
 		nuevo_nodo->elemento = elemento;
 		nuevo_nodo->izquierda = NULL;
@@ -31,28 +32,32 @@ nodo_abb_t *inserto_elemento_y_comparo(abb_t *arbol, nodo_abb_t *nodo_actual, vo
 
 	int comparacion = arbol->comparador(elemento, nodo_actual->elemento);
 	if (comparacion > 0)
-		nodo_actual->derecha = inserto_elemento_y_comparo(arbol, nodo_actual->derecha, elemento);
+		nodo_actual->derecha = inserto_elemento_y_comparo(
+			arbol, nodo_actual->derecha, elemento);
 
-	if(comparacion <= 0)//asumo que cuando el elemento es igual, va a la izquierda
-		nodo_actual->izquierda = inserto_elemento_y_comparo(arbol, nodo_actual->izquierda, elemento);
+	if (comparacion <=
+	    0) //asumo que cuando el elemento es igual, va a la izquierda
+		nodo_actual->izquierda = inserto_elemento_y_comparo(
+			arbol, nodo_actual->izquierda, elemento);
 
 	return nodo_actual;
 }
 
 abb_t *abb_insertar(abb_t *arbol, void *elemento)
 {
-	if(arbol == 0)
+	if (arbol == 0)
 		return NULL;
-	if(elemento == 0)//re-verrrr  PD:borrar comentarios
+	if (elemento == 0) //re-verrrr  PD:borrar comentarios
 		return NULL;
-	arbol->nodo_raiz = inserto_elemento_y_comparo(arbol, arbol->nodo_raiz, elemento);
+	arbol->nodo_raiz =
+		inserto_elemento_y_comparo(arbol, arbol->nodo_raiz, elemento);
 	return arbol;
 }
 
 nodo_abb_t *buscar_menor_predecesor(nodo_abb_t *raiz)
 {
 	nodo_abb_t *nodo_menor = NULL;
-	if(!raiz->derecha->derecha) {
+	if (!raiz->derecha->derecha) {
 		nodo_menor = raiz->derecha;
 		raiz->derecha = nodo_menor->izquierda;
 		return nodo_menor;
@@ -63,7 +68,7 @@ nodo_abb_t *buscar_menor_predecesor(nodo_abb_t *raiz)
 nodo_abb_t *reemplazar_con_menor_predecesor(nodo_abb_t *raiz)
 {
 	nodo_abb_t *aux = NULL;
-	if(!raiz->izquierda->derecha) {
+	if (!raiz->izquierda->derecha) {
 		aux = raiz->izquierda;
 		aux->derecha = raiz->derecha;
 		return aux;
@@ -74,21 +79,23 @@ nodo_abb_t *reemplazar_con_menor_predecesor(nodo_abb_t *raiz)
 	return aux;
 }
 
-nodo_abb_t *saco_al_elemento_comparando(abb_t *arbol, nodo_abb_t *nodo_actual, void *elemento, void **elemento_encontrado)
+nodo_abb_t *saco_al_elemento_comparando(abb_t *arbol, nodo_abb_t *nodo_actual,
+					void *elemento,
+					void **encontre_al_elemento)
 {
-	if(nodo_actual == 0)
+	if (!nodo_actual)
 		return NULL;
 	int comparacion = arbol->comparador(elemento, nodo_actual->elemento);
-	if(comparacion == 0) {
+	if (comparacion == 0) {
 		nodo_abb_t *aux = NULL;
-		*elemento_encontrado = nodo_actual->elemento;
-		if(nodo_actual->izquierda && nodo_actual->derecha) {
+		*encontre_al_elemento = nodo_actual->elemento;
+		if (nodo_actual->izquierda && nodo_actual->derecha) {
 			aux = reemplazar_con_menor_predecesor(nodo_actual);
 			free(nodo_actual);
 			arbol->tamanio--;
 			return aux;
 		}
-		if(nodo_actual->izquierda) {
+		if (nodo_actual->izquierda) {
 			aux = nodo_actual->izquierda;
 			free(nodo_actual);
 			arbol->tamanio--;
@@ -99,20 +106,25 @@ nodo_abb_t *saco_al_elemento_comparando(abb_t *arbol, nodo_abb_t *nodo_actual, v
 		arbol->tamanio--;
 		return aux;
 	}
-	if(comparacion > 0)
-		nodo_actual->derecha = saco_al_elemento_comparando(arbol, nodo_actual->derecha, elemento, elemento_encontrado);
-	if(comparacion < 0)
-		nodo_actual->izquierda = saco_al_elemento_comparando(arbol, nodo_actual->izquierda, elemento, elemento_encontrado);
-
+	if (comparacion > 0)
+		nodo_actual->derecha = saco_al_elemento_comparando(
+			arbol, nodo_actual->derecha, elemento,
+			encontre_al_elemento);
+	if (comparacion < 0)
+		nodo_actual->izquierda = saco_al_elemento_comparando(
+			arbol, nodo_actual->izquierda, elemento,
+			encontre_al_elemento);
 	return nodo_actual;
 }
 
 void *abb_quitar(abb_t *arbol, void *elemento)
 {
-	if(arbol == 0 || abb_vacio(arbol))
+	if (arbol == 0 || abb_vacio(arbol))
 		return NULL;
 	void *encontre_al_elemento = NULL;
-	arbol->nodo_raiz = saco_al_elemento_comparando(arbol, arbol->nodo_raiz, elemento, &encontre_al_elemento);
+	arbol->nodo_raiz = saco_al_elemento_comparando(
+		arbol, arbol->nodo_raiz, elemento, &encontre_al_elemento);
+
 	return encontre_al_elemento;
 }
 
@@ -123,7 +135,9 @@ void *abb_buscar(abb_t *arbol, void *elemento)
 
 bool abb_vacio(abb_t *arbol)
 {
-	return true;
+	if (!arbol || (!arbol->nodo_raiz) || (arbol->tamanio == 0))
+		return true;
+	return false;
 }
 
 size_t abb_tamanio(abb_t *arbol)
@@ -131,12 +145,34 @@ size_t abb_tamanio(abb_t *arbol)
 	return 0;
 }
 
+void destruir_post_orden(nodo_abb_t *nodo_actual, void (*destructor)(void *))
+{
+	if (!nodo_actual)
+		return;
+	destruir_post_orden(nodo_actual->izquierda, destructor);
+	destruir_post_orden(nodo_actual->derecha, destructor);
+	if (destructor && nodo_actual->elemento)
+		destructor(nodo_actual->elemento);
+	free(nodo_actual);
+	return;
+}
 void abb_destruir(abb_t *arbol)
 {
+	if (arbol == 0)
+		return;
+	destruir_post_orden(arbol->nodo_raiz, NULL);
+	free(arbol);
 }
 
 void abb_destruir_todo(abb_t *arbol, void (*destructor)(void *))
 {
+	if (arbol == 0)
+		return;
+	if (destructor == 0)
+		destruir_post_orden(arbol->nodo_raiz, NULL);
+	else
+		destruir_post_orden(arbol->nodo_raiz, destructor);
+	free(arbol);
 }
 
 size_t abb_con_cada_elemento(abb_t *arbol, abb_recorrido recorrido,
