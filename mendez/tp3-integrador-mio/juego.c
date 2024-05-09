e778bf50e42e57029e7bea7c47ecda645d541960
juanchitho <jbauti9@gmail.com>
2024-05-09 20:38:06 -0300

    consignas del tp3

---
+++ mendez/tp3-integrador-mio/juego.c
@@ -0,0 +1,14 @@
+#include "src/tp.h"
+
+int main(int argc, char const *argv[])
+{
+	//Se recomienda pasar el archivo de texto con los pokemon como argumento al ejecutar el programa
+
+	//crear el juego
+	tp_crear(argv[1]);
+	//mostrar menues
+	//correr carrera
+	//etc
+
+	return 0;
+}
---
+++ mendez/tp3-integrador-mio/src/tp.c
@@ -0,0 +1,69 @@
+#include "tp.h"
+#include <stdlib.h>
+
+TP *tp_crear(const char *nombre_archivo)
+{
+	return NULL;
+}
+
+int tp_cantidad_pokemon(TP *tp)
+{
+	return 0;
+}
+
+const struct pokemon_info *tp_buscar_pokemon(TP *tp, const char *nombre)
+{
+	return NULL;
+}
+
+char *tp_nombres_disponibles(TP *tp)
+{
+	return NULL;
+}
+
+bool tp_seleccionar_pokemon(TP *tp, enum TP_JUGADOR jugador, const char *nombre)
+{
+	return false;
+}
+
+const struct pokemon_info *tp_pokemon_seleccionado(TP *tp,
+						   enum TP_JUGADOR jugador)
+{
+	return NULL;
+}
+
+unsigned tp_agregar_obstaculo(TP *tp, enum TP_JUGADOR jugador,
+			      enum TP_OBSTACULO obstaculo, unsigned posicion)
+{
+	return 0;
+}
+
+unsigned tp_quitar_obstaculo(TP *tp, enum TP_JUGADOR jugador, unsigned posicion)
+{
+	return 0;
+}
+
+char *tp_obstaculos_pista(TP *tp, enum TP_JUGADOR jugador)
+{
+	return NULL;
+}
+
+void tp_limpiar_pista(TP *tp, enum TP_JUGADOR jugador)
+{
+	return;
+}
+
+unsigned tp_calcular_tiempo_pista(TP *tp, enum TP_JUGADOR jugador)
+{
+	return 0;
+}
+
+char *tp_tiempo_por_obstaculo(TP *tp, enum TP_JUGADOR jugador)
+{
+	return NULL;
+}
+
+void tp_destruir(TP *tp)
+{
+	return;
+}
