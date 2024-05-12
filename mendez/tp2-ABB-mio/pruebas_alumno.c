b2713c3240b55c62431e3ac591372c92b1b645b6
juanchitho <jbauti9@gmail.com>
2024-05-12 14:50:17 -0300

    agrego algunos test

--- mendez/tp2-ABB-mio/pruebas_alumno.c
+++ mendez/tp2-ABB-mio/pruebas_alumno.c
@@ -68,16 +68,6 @@ void prueba_de_insertar_elementos_en_un_arbol_y_chequeo_de_tamanio()
 	abb_destruir(nuevo_arbol);
 }
 
-void prueba_de_insertar_un_elemento_null_en_un_arbol()
-{
-	abb_t *arbol = abb_crear(comparador);
-	abb_t *nuevo_arbol = abb_insertar(arbol, NULL);
-	pa2m_afirmar(
-		nuevo_arbol == NULL,
-		"No se puede insertar el elemento , ya que el elemento es NULL");
-	abb_destruir(nuevo_arbol);
-	abb_destruir(arbol);
-}
 void prueba_de_insertar_elemento_en_un_arbol_que_es_null()
 {
 	int numero = 31;
@@ -121,6 +111,193 @@ void prueba_de_eliminar_un_elemento_que_no_existe_en_el_arbol()
 	abb_destruir(nuevo_arbol);
 }
 
+void prueba_de_encontrar_un_elemento_en_el_arbol()
+{
+	int numero_25 = 25;
+	int numero_31 = 31;
+	abb_t *nuevo_arbol = abb_crear(comparador);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_25);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_31);
+	pa2m_afirmar(abb_buscar(nuevo_arbol, &numero_31) == &numero_31,
+		     "Se encontro el primer elemento correctamente");
+	pa2m_afirmar(abb_buscar(nuevo_arbol, &numero_25) == &numero_25,
+		     "Se encontro el segundo elemento correctamente");
+	abb_destruir(nuevo_arbol);
+}
+
+void prueba_de_encontrar_un_elemento_que_no_existe_en_el_arbol()
+{
+	int numero_25 = 25;
+	int numero_31 = 31;
+	abb_t *nuevo_arbol = abb_crear(comparador);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_25);
+	pa2m_afirmar(
+		abb_buscar(nuevo_arbol, &numero_31) == NULL,
+		"El elemento no existe en el arbol, por lo tanto no se puede encontrar");
+	abb_destruir(nuevo_arbol);
+}
+
+void puedo_encontrar_elemento()
+{
+	int numero_10 = 10;
+	int numero_20 = 20;
+	abb_t *nuevo_arbol = abb_crear(comparador);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_10);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_20);
+	pa2m_afirmar(abb_buscar(nuevo_arbol, &numero_20) == &numero_20,
+		     "Encontro el primer elemento buscado");
+	pa2m_afirmar(abb_buscar(nuevo_arbol, &numero_10) == &numero_10,
+		     "Encontro el segundo elemento buscado");
+	abb_destruir(nuevo_arbol);
+}
+
+void itero_hasta_encontrar_null()
+{
+	int numero_12 = 12;
+	int numero_18 = 18;
+	int numero_4 = 4;
+	int numero_3 = 3;
+	int numero_8 = 8;
+	void *null = NULL;
+	abb_t *nuevo_arbol = abb_crear(comparador);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_12);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_18);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_4);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_3);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_8);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &null);
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, INORDEN,
+					   imprimir_numeros, &null) == 1,
+		     "Itero 1 vez hasta encontrar NULL INORDEN");
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, PREORDEN,
+					   imprimir_numeros, &null) == 4,
+		     "Itero 4 veces hasta encontrar NULL PREORDEN");
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, POSTORDEN,
+					   imprimir_numeros, &null) == 1,
+		     "Itero 1 vez hasta encontrar NULL POSTORDEN");
+	abb_destruir(nuevo_arbol);
+}
+void no_es_posible_iterar_con_un_arbol_que_es_null()
+{
+	void *null = NULL;
+	pa2m_afirmar(abb_con_cada_elemento(NULL, INORDEN, imprimir_numeros,
+					   &null) == 0,
+		     "No se puedo iterar un arbol que es NULL");
+}
+void no_es_posible_iterar_con_una_funcion_null()
+{
+	abb_t *nuevo_arbol = abb_crear(comparador);
+	void *null = NULL;
+	nuevo_arbol = abb_insertar(nuevo_arbol, &null);
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, INORDEN, NULL, &null) ==
+			     0,
+		     "No se puede iterar arbol con funcion NULL");
+	abb_destruir(nuevo_arbol);
+}
+
+void itero_hasta_encontrar_numero_inorden()
+{
+	int numero_10 = 10;
+	int numero_20 = 20;
+	int numero_5 = 5;
+	int numero_3 = 3;
+	int numero_7 = 7;
+	int numero_15 = 15;
+	int numero_25 = 25;
+	abb_t *nuevo_arbol = abb_crear(comparador);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_10);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_20);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_5);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_3);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_7);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_15);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_25);
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, INORDEN,
+					   imprimir_numeros, &numero_25) == 7,
+		     "Itero 7 veces hasta encontrar el 25");
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, INORDEN,
+					   imprimir_numeros, &numero_10) == 4,
+		     "Itero 4 veces hasta encontrar el 10");
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, INORDEN,
+					   imprimir_numeros, &numero_15) == 5,
+		     "Itero 5 veces hasta encontrar el 15");
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, INORDEN,
+					   imprimir_numeros, &numero_20) == 6,
+		     "Itero 6 veces hasta encontrar el 20");
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, INORDEN,
+					   imprimir_numeros, &numero_5) == 2,
+		     "Itero 2 veces hasta encontrar el 5");
+	abb_destruir(nuevo_arbol);
+}
+void itero_hasta_encontrar_numero_preorden()
+{
+	int numero_10 = 10;
+	int numero_20 = 20;
+	int numero_5 = 5;
+	int numero_3 = 3;
+	int numero_7 = 7;
+	int numero_15 = 15;
+	int numero_25 = 25;
+	abb_t *nuevo_arbol = abb_crear(comparador);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_10);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_20);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_5);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_3);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_7);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_15);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_25);
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, PREORDEN,
+					   imprimir_numeros, &numero_25) == 7,
+		     "Itero 7 veces hasta encontrar el 25");
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, PREORDEN,
+					   imprimir_numeros, &numero_10) == 1,
+		     "Itero 1 veces hasta encontrar el 10");
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, PREORDEN,
+					   imprimir_numeros, &numero_15) == 6,
+		     "Itero 6 veces hasta encontrar el 15");
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, PREORDEN,
+					   imprimir_numeros, &numero_20) == 5,
+		     "Itero 5 veces hasta encontrar el 20");
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, PREORDEN,
+					   imprimir_numeros, &numero_5) == 2,
+		     "Itero 2 veces hasta encontrar el 5");
+	abb_destruir(nuevo_arbol);
+}
+void itero_hasta_encontrar_numero_postorden()
+{
+	int numero_10 = 10;
+	int numero_20 = 20;
+	int numero_5 = 5;
+	int numero_3 = 3;
+	int numero_7 = 7;
+	int numero_15 = 15;
+	int numero_25 = 25;
+	abb_t *nuevo_arbol = abb_crear(comparador);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_10);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_20);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_5);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_3);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_7);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_15);
+	nuevo_arbol = abb_insertar(nuevo_arbol, &numero_25);
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, POSTORDEN,
+					   imprimir_numeros, &numero_25) == 5,
+		     "Itero 5 veces hasta encontrar el 25");
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, POSTORDEN,
+					   imprimir_numeros, &numero_10) == 7,
+		     "Itero 7 veces hasta encontrar el 10");
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, POSTORDEN,
+					   imprimir_numeros, &numero_15) == 4,
+		     "Itero 4 veces hasta encontrar el 15");
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, POSTORDEN,
+					   imprimir_numeros, &numero_20) == 6,
+		     "Itero 5 veces hasta encontrar el 20");
+	pa2m_afirmar(abb_con_cada_elemento(nuevo_arbol, POSTORDEN,
+					   imprimir_numeros, &numero_5) == 3,
+		     "Itero 2 veces hasta encontrar el 5");
+	abb_destruir(nuevo_arbol);
+}
+
 int main()
 {
 	pa2m_nuevo_grupo(
@@ -138,7 +315,6 @@ int main()
 		"\n======================== XXX ========================");
 	pa2m_nuevo_grupo("Pruebas del tipo de insercion al ABB");
 	prueba_de_insertar_elementos_en_un_arbol_y_chequeo_de_tamanio();
-	//prueba_de_insertar_un_elemento_null_en_un_arbol();
 	prueba_de_insertar_elemento_en_un_arbol_que_es_null();
 
 	pa2m_nuevo_grupo(
@@ -149,9 +325,17 @@ int main()
 
 	pa2m_nuevo_grupo(
 		"\n======================== XXX ========================");
+	pa2m_nuevo_grupo("Pruebas de busqueda en el ABB");
+	prueba_de_encontrar_un_elemento_en_el_arbol();
+	prueba_de_encontrar_un_elemento_que_no_existe_en_el_arbol();
 
 	pa2m_nuevo_grupo(
 		"\n======================== XXX ========================");
+	pa2m_nuevo_grupo("Pruebas del iterador");
+	itero_hasta_encontrar_null();
+	no_es_posible_iterar_con_un_arbol_que_es_null();
+	no_es_posible_iterar_con_una_funcion_null();
+
 	pa2m_nuevo_grupo(
 		"\n======================== XXX ========================");
 	return pa2m_mostrar_reporte();
