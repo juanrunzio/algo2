8bf982fc8f3779b74d264469d9b38aabdc01660d
juanrunzio <jbauti9@gmail.com>
2024-06-02 19:47:52 -0300

    testeando abrir un .txt

--- mendez/tp3-integrador-mio/juego.c
+++ mendez/tp3-integrador-mio/juego.c
@@ -10,5 +10,29 @@ int main(int argc, char const *argv[])
 	//correr carrera
 	//etc
 
+
+	//-----------------test-----------------------
+// Función para imprimir todos los pokémon en orden alfabético
+static void imprimir_pokemons_recursivo(nodo_abb_t *raiz) {
+    if (raiz != NULL) {
+        imprimir_pokemons_recursivo(raiz->izquierda);
+        printf("Nombre: %s, Fuerza: %d, Destreza: %d, Inteligencia: %d\n",
+               raiz->pokemon->nombre, raiz->pokemon->fuerza,
+               raiz->pokemon->destreza, raiz->pokemon->inteligencia);
+        imprimir_pokemons_recursivo(raiz->derecha);
+    }
+}
+
+// Función pública para imprimir todos los pokémon en orden alfabético
+void imprimir_pokemons(TP *tp) {
+    if (tp == NULL || tp->raiz == NULL) {
+        printf("No hay pokémon para imprimir.\n");
+    } else {
+        imprimir_pokemons_recursivo(tp->raiz);
+    }
+}
+
+//-----------------test-----------------------
+
 	return 0;
 }
